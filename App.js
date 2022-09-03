58a45ba6be8e81e9ff56bfb72149dbe923f4f113
erik <erik_g@web.de>
2022-09-03 10:20:05 +0300

    Test

---
+++ App.js
@@ -0,0 +1,33 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button, Alert, TextInput} from 'react-native';
+import React, {useState} from 'react';
+ 
+export default function App() {
+
+  const [text, setText] = useState('');
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+          style={{width:
+            200,
+            borderColor:'gray',
+            borderWidth:1
+            }}
+          onChangeText={text => setText(text)}
+          value={text}
+      />
+      <Button title="Press me" onPress={() => Alert.alert('You typed: ' + text)}/>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
